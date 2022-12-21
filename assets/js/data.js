3ff44450c5db824e0e17c9663b7bf8d28124050a
hajjou walkadi <walkadihajjou153@gmail.com>
2022-12-21 19:45:23 +0100

    working on cards

---
+++ assets/js/data.js
@@ -0,0 +1,26 @@
+questions = [
+    {
+        question : "question 1 ", 
+        options : [
+            "option 1 ", 
+            "option 2 ", 
+            "option 3 ",
+        ]
+    },
+    {
+        question : "question 2 ", 
+        options : [
+            "option 1 ", 
+            "option 2 ", 
+            "option 3 ",
+        ]
+    },
+    {
+        question : "question 3 ", 
+        options : [
+            "option 1 ", 
+            "option 2 ", 
+            "option 3 ",
+        ]
+    },
+]
