60ac9735642fa5e29dcbe9880c21bc8a5dbbe397
hajjou walkadi <walkadihajjou153@gmail.com>
2022-12-23 16:40:16 +0100

    QUIZIZY-1 all pages done (styling)

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,5 @@
-
-let index = 0;
+ 
+let index ,quiz_questions;
 let optionsElement1 = document.getElementById('options1');
 let optionsElement2 = document.getElementById('options2');
 let optionsElement3 = document.getElementById('options3');
@@ -8,6 +8,7 @@ let questionElement = document.querySelector('#question');
 let num_result=document.getElementById("result-num");
 let AllAnswers=document.getElementById("cartes-container");
 let step2=document.getElementById("step2");
+let step3=document.getElementById("step3");
 let progressing =document.getElementById("progressing");
 // function shuffle(array) {
 //     for (let i = array.length - 1; i > 0; i--) {
@@ -16,30 +17,30 @@ let progressing =document.getElementById("progressing");
 //     }
 //     return array;
 //   }
-
-let random = questions.sort(()=> Math.random() - .5);
+let circle2 = document.getElementById("corcile2");
+let circle3 = document.getElementById("corcile3");
+let showResult =document.getElementById("showResult");
+let resultCard = document.getElementById("result-card");
 function startQuiz(){
-        
-    // if( answer ) index ++;
 
-    // if (index = max ) stop 
-
-    start(index);
+    start();
 }
 function nextQuiz(){
+    nexttt();
     index++;
-    start(index);
 }
 
-let quiz_questions = random;
 
 function start(){
             
     informationCard = document.querySelector('.information-card').style.display = "none";
     questionCard = document.querySelector('.questions-card').style.display = "block";
-    show(index);
+    showResult.style.display="none";
+    quiz_questions =  questions.sort(()=> Math.random() - .5);
+    index = 0;
     step2.classList.add('greencolor');
-
+    circle2.classList.add("activeStep");
+    nexttt();
     // for (let index = 0; index < options.length; index++) {
     // optionsElement.innerHTML += `<div class="form-check">
     //         <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
@@ -50,41 +51,42 @@ function start(){
     // }
 }
 
-function check(e){
-    // console.log(e.textContent);
+function check(){
     nextQuiz();
 }
 
-function show(qIndex){
+function show(question){
    
-    optionsElement1.innerText = quiz_questions[qIndex].options[0];        
-    optionsElement2.innerText = quiz_questions[qIndex].options[1];        
-    optionsElement3.innerText = quiz_questions[qIndex].options[2];        
-    optionsElement4.innerText = quiz_questions[qIndex].options[3];        
-
-    questionElement.innerText= quiz_questions[qIndex].question;
-    num_result.innerText=qIndex+1;
-    for(let i=0;i<qIndex+2;i++){
+    optionsElement1.innerText = question.options[0];        
+    optionsElement2.innerText = question.options[1];        
+    optionsElement3.innerText = question.options[2];        
+    optionsElement4.innerText = question.options[3];
+    questionElement.innerText= question.question;
+    num_result.innerText=index+1;
+    for(let i=0;i<index+2;i++){
         progressing.style.width = i*3.5+"rem";
     }
 
 }
 function nexttt(){
-    show(index);
-    resetAll();
-}
-
-//progress bar
-var progress = 50; // current progress
-var progressBar = document.querySelector('.progress-bar');
-progressBar.setAttribute('aria-valuenow', progress);
-progressBar.style.width = progress + '%';
-
-function resetAll(){
-    while(AllAnswers.firstChild){
-        AllAnswers.removeChild(AllAnswers.firstChild)
+    show(quiz_questions[index]);
+    if(index > 8){ 
+        showResult.style.display="block";
     }
+    // resetAll();
 }
+
+// //progress bar
+// var progress = 50; // current progress
+// var progressBar = document.querySelector('.progress-bar');
+// progressBar.setAttribute('aria-valuenow', progress);
+// progressBar.style.width = progress + '%';
+
+// function resetAll(){
+//     while(AllAnswers.firstChild){
+//         AllAnswers.removeChild(AllAnswers.firstChild)
+//     }
+// }
 
 
 let bodyContainer = document.getElementsByTagName('container');
@@ -109,4 +111,13 @@ function changeContenu(index){
     bodyContainer[index].style.display = "block";
     btnCircleChose[index].style.color = "green";
 
+}
+
+function ShowResults(){
+    console.log("bkbkb")
+    informationCard = document.querySelector('.information-card').style.display = "none";
+    questionCard = document.querySelector('.questions-card').style.display = "none";
+    resultCard.style.display="block";
+    step3.classList.add('greencolor');
+    circle3.classList.add("activeStep");
 }
